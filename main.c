c26867f661e2ea6d8d22b2722b78233d659b2b06
RealStoneSword <58371315+RealStoneSword@users.noreply.github.com>
2020-08-20 00:00:50 -0500

    Update main.c

--- main.c
+++ main.c
@@ -5,12 +5,47 @@ int main() {
     int choice1;
     char choice2;
     char choice3;
+     /* A stands for Add, id stands for sale id */
+    char choice3aid[255];
+    /* PID stands for Product id */
+    char choice3apid[255];
+    /* SA stands for sold amount */
+    char choice3asa[255];
     printf("Store Management System\n\nWhat would you like to do?\n1.Add/Remove/list Product(s)\n2.Add/List/Remove Sales\n");
     scanf("%d", &choice1);
     
-    /* The if statement is choice2,
-    the else if statment if choice3 */
+    /* The case '1' statement is for choice2,
+    the case '2' statment is for choice3 */
     
+    switch(choice1){
+    case 1 :
+        printf("Would you like to (A)dd, (R)emove, or (l)ist products?\n");
+        scanf("%s", &choice2);
+        break;
+    case 2 :
+        printf("Would you like too (A)dd, (L)ist, or (R)emove sales?\n");
+        scanf("%s", &choice3);
+            switch (choice3)
+            {
+            case 'a' :
+                printf("What would you like the ID of the sale you are adding going to be?\n(if you dont know, list the sales list and look for the greatest ID and add 1.)\n");
+                scanf("%s", &choice3aid);
+                printf("What is the product that you sold id?\n(if you dont know, list the produts and look for the right product's id)\n");
+                scanf("%s", &choice3apid);
+                printf("How many products did you sell?");
+                break;
+            
+            default :
+                printf("Invalid Choice. Please try again.\n");
+            }
+        break;
+    default :
+        printf("Invalid Choice. Please try again.\n");
+    }
+
+    /* The old if statement (here for future use if needed) */
+
+    /*
     if(choice1 == 1) {
         printf("Would you like to (A)dd, (R)emove, or (l)ist products?\n");
         scanf("%s", &choice2);
@@ -24,5 +59,7 @@ int main() {
     else{
         printf("Invalid Choice. Please try again.\n");
     }
+    */
+
     return 0;
 }
